968fa14d76d3de421842ac8bac927ccfccfa0beb
dentex <83866386+dentex22@users.noreply.github.com>
2021-09-19 21:02:19 +0300

    updates

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+import cv2
+import numpy as np
+import glob
+import os
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+logo = cv2.imread("elite_logo.jpg")
+img = cv2.imread("flowers\sunflower.jpg")
+cv2.imshow("Img", img)
+cv2.imshow("Logo", logo)
+cv2.waitKey(0)
